492afdc83fa939d931e524818e05411a586ae6b4
rouming <work@work>
2012-07-26 17:56:35 +0400

    supress all 'left shift count >= width of type' warnings on avr-gcc

--- src/raytri.h
+++ src/raytri.h
@@ -7,10 +7,10 @@ typedef int32_t    fp_t;
 #define FPP        16
 #define FPMUL(x,y)	((fp_t)(((int64_t)(x) * (int64_t)(y)) >> FPP))
 #define FPDIV(x,y)	((fp_t)(((int64_t)(x) << FPP) / (y)))
-#define FTOFP(x)    ((fp_t)((x) * (1<<FPP) + ((x) >= 0 ? 0.5 : -0.5)))
-#define FPTOF(x)    ((float)(x) / (1<<FPP))
-#define FPONE       (fp_t)(1<<FPP)
-#define FPZERO      (fp_t)0
+#define FTOFP(x)    ((fp_t)((x) * ((fp_t)1<<FPP) + ((x) >= 0 ? 0.5 : -0.5)))
+#define FPTOF(x)    ((float)(x) / ((fp_t)1<<FPP))
+#define FPONE       ((fp_t)1<<FPP)
+#define FPZERO      ((fp_t)0)
 
 extern int fixed_intersect_triangle(
 	fp_t orig[3], fp_t dir[3],
