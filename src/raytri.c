731998066ed6056d43d32e92c5c82fff749a8be3
rouming <work@work>
2012-08-01 19:51:12 +0400

    reduce fixed point precision from 16 bit to 8 to reduce code size generation
    also reduce prevision of fixed epsilon error, because for 8 bit
    it become zero

--- src/raytri.c
+++ src/raytri.c
@@ -7,8 +7,11 @@
 #include <math.h>
 
 #include "raytri.h"
+#include "std.h"
 
 #define EPSILON 0.0001f
+BUILD_BUG_ON(EPSILON == 0);
+
 #define CROSS(dest,v1,v2)						\
 	dest[0]=v1[1]*v2[2]-v1[2]*v2[1];			\
 	dest[1]=v1[2]*v2[0]-v1[0]*v2[2];			\
@@ -19,8 +22,10 @@
 	dest[1]=v1[1]-v2[1];						\
 	dest[2]=v1[2]-v2[2];
 
-#define FIXED_EPSILON FTOFP(0.0001f)
-#define FIXED_CROSS(dest,v1,v2)								\
+#define FIXED_EPSILON FTOFP(0.01f)
+BUILD_BUG_ON(FIXED_EPSILON == 0);
+
+#define FIXED_CROSS(dest,v1,v2)							\
 	dest[0]=FPMUL(v1[1],v2[2])-FPMUL(v1[2],v2[1]);		\
 	dest[1]=FPMUL(v1[2],v2[0])-FPMUL(v1[0],v2[2]);		\
 	dest[2]=FPMUL(v1[0],v2[1])-FPMUL(v1[1],v2[0]);
