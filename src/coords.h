52602c98e79f43374afb19a1f008ab4f088e320a
rouming <work@work>
2012-08-01 11:35:46 +0400

    add map of dodecahedron vertexes to real tlc pinouts

--- src/coords.h
+++ src/coords.h
@@ -7,28 +7,21 @@
 #include "fixed.h"
 
 /*
- * PINOUT FOR ONE TLC
- *  ------------------
- *
- * 15|14|13|12|11|10| 9| 8| 7| 6| 5| 4| 3| 2| 1| 0|
- *  G| B| R| G| B| R| G| B| R| G| B| R| G| B| R|
- *
- *
- * BOTTOM
+ * BOTTOM (clockwise direction)
  * ------
  *
  *
- *    @ 8             @ 9
+ *    @8             @9
  *
- *        @ 3      @ 4
+ *        @3      @4
  *
  *
- *      @ 2         @ 5
- *  @ 7                 @ 10
+ *      @2         @5
+ *  @7                 @10
  *
- *            @ 1
+ *            @1
  *
- *            @ 6
+ *            @6
  *
  *
  * ^    ^    ^    ^    ^    ^
@@ -36,19 +29,24 @@
  *
  *            VIEWER
  *
- * TOP
+ * leds verteces:
+ *    TLC0: @1->0, @2->18, @3->9,  @4->11, @5->14
+ *    TLC1: @6->2, @7->7,  @8->19, @9->15, @10->5
+ *
+ *
+ * TOP (clockwise direction)
  * ---
  *
- *            @ 6
+ *            @6
  *
- *            @ 1
- *  @ 10                @ 7
- *      @ 5         @ 2
+ *            @1
+ *  @10                @7
+ *      @5         @2
  *
  *
- *       @ 4     @ 3
+ *       @4     @3
  *
- *    @ 9            @ 8
+ *    @9            @8
  *
  *
  * ^    ^    ^    ^    ^    ^
@@ -56,13 +54,10 @@
  *
  *            VIEWER
  *
+ * leds verteces:
+ *    TLC1: @1->3, @2->13, @3->10,  @4->8,  @5->17
+ *    TLC2: @6->1, @7->4,  @8->12,  @9->16, @10->6
  */
-
-/* TLC5940 pinout index */
-#define TLC0 16*0
-#define TLC1 16*1
-#define TLC2 16*2
-#define TLC3 16*3
 
 /* Dodecahedron 20 real coords verteces: every row is a vertex (x,y,z).
  * Original coords were taken from
@@ -100,36 +95,47 @@ const static fp_t s_coords_vert[] =
 	FTOFP(-0.57735f),		FTOFP(-0.18760f),		FTOFP(-0.79465f),
 };
 
-/* Dodecahedron 20 leds verteces: every row is a led index (r,g,b)  */
+/*
+ * PINOUT FOR ONE TLC
+ *  ------------------
+ *
+ * 15|14|13|12|11|10| 9| 8| 7| 6| 5| 4| 3| 2| 1| 0|
+ *  G| B| R| G| B| R| G| B| R| G| B| R| G| B| R|
+ */
+#define R_TLC(tlc, led) (tlc*16 + led*3 + 1)
+#define G_TLC(tlc, led) (tlc*16 + led*3 + 3)
+#define B_TLC(tlc, led) (tlc*16 + led*3 + 2)
+
+/* Dodecahedron 20 leds verteces: every row is a tlc led (r,g,b)  */
 const static uint8_t s_leds_vert[] =
 {
 	/*  #0 TLC5940 */
-	TLC0+  1,  TLC0+  2,  TLC0+  3,
-	TLC0+  4,  TLC0+  5,  TLC0+  6,
-	TLC0+  7,  TLC0+  8,  TLC0+  9,
-	TLC0+ 10,  TLC0+ 11,  TLC0+ 12,
-	TLC0+ 13,  TLC0+  4,  TLC0+ 15,
+	R_TLC(0, 0),	G_TLC(0, 0),	B_TLC(0, 0),
+	R_TLC(3, 0),	G_TLC(3, 0),	B_TLC(3, 0),
+	R_TLC(1, 0),	G_TLC(1, 0),	B_TLC(1, 0),
+	R_TLC(2, 0),	G_TLC(2, 0),	B_TLC(2, 0),
+	R_TLC(3, 1),	G_TLC(3, 1),	B_TLC(3, 1),
 
 	/*  #1 TLC5940 */
-	TLC1+  1,  TLC1+  2,  TLC1+  3,
-	TLC1+  4,  TLC1+  5,  TLC1+  6,
-	TLC1+  7,  TLC1+  8,  TLC1+  9,
-	TLC1+ 10,  TLC1+ 11,  TLC1+ 12,
-	TLC1+ 13,  TLC1+  4,  TLC1+ 15,
+	R_TLC(1, 4),	G_TLC(1, 4),	B_TLC(1, 4),
+	R_TLC(3, 4),	G_TLC(3, 4),	B_TLC(3, 4),
+	R_TLC(1, 1),	G_TLC(1, 1),	B_TLC(1, 1),
+	R_TLC(2, 3),	G_TLC(2, 3),	B_TLC(2, 3),
+	R_TLC(0, 2),	G_TLC(0, 2),	B_TLC(0, 2),
 
 	/*  #2 TLC5940 */
-	TLC2+  1,  TLC2+  2,  TLC2+  3,
-	TLC2+  4,  TLC2+  5,  TLC2+  6,
-	TLC2+  7,  TLC2+  8,  TLC2+  9,
-	TLC2+ 10,  TLC2+ 11,  TLC2+ 12,
-	TLC2+ 13,  TLC2+  4,  TLC2+ 15,
+	R_TLC(2, 2),	G_TLC(2, 2),	B_TLC(2, 2),
+	R_TLC(0, 3),	G_TLC(0, 3),	B_TLC(0, 3),
+	R_TLC(3, 2),	G_TLC(3, 2),	B_TLC(3, 2),
+	R_TLC(2, 1),	G_TLC(2, 1),	B_TLC(2, 1),
+	R_TLC(0, 4),	G_TLC(0, 4),	B_TLC(0, 4),
 
 	/*  #3 TLC5940 */
-	TLC3+  1,  TLC3+  2,  TLC3+  3,
-	TLC3+  4,  TLC3+  5,  TLC3+  6,
-	TLC3+  7,  TLC3+  8,  TLC3+  9,
-	TLC3+ 10,  TLC3+ 11,  TLC3+ 12,
-	TLC3+ 13,  TLC3+  4,  TLC3+ 15
+	R_TLC(1, 3),	G_TLC(1, 3),	B_TLC(1, 3),
+	R_TLC(3, 3),	G_TLC(3, 3),	B_TLC(3, 3),
+	R_TLC(2, 4),	G_TLC(2, 4),	B_TLC(2, 4),
+	R_TLC(0, 1),	G_TLC(0, 1),	B_TLC(0, 1),
+	R_TLC(1, 2),	G_TLC(1, 2),	B_TLC(1, 2),
 };
 
 /* Dodecahedron triangle faces from verteces,
@@ -150,24 +156,6 @@ const static uint8_t s_vert_tri_faces[] =
 	3,  17, 8,      3,  8,  10,     3,   10,  13,
 	11, 9,  19,     11, 19, 1,      11,  1,   15,
 	9,  18, 7,      9,  7,  6,      9,   6,   19
-};
-
-/* Dodecahedron 12 pentagons from verteces,
-   i.e. one row in array is a pentagon */
-const static uint8_t s_vert_penta_faces[] =
-{
-	12,  2,  0, 14,  5,
-	10,  8, 16,  2, 12,
-	17,  6,  7, 16,  8,
-	16,  7, 18,  0,  2,
-	0, 18,  9, 11, 14,
-	5, 14, 11, 15,  4,
-	13,  4, 15,  1,  3,
-	17,  3,  1, 19,  6,
-	10, 12,  5,  4, 13,
-	3, 17,  8, 10, 13,
-	11,  9, 19,  1, 15,
-	9, 18,  7,  6, 19
 };
 
 #endif //COORDS_H
