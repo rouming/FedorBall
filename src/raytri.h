ebaaa2fa1aed58afa5a0eb66c4d74582a1d9168f
rouming <work@work>
2012-08-01 14:17:59 +0400

    add extern "C" to raytri.h

--- src/raytri.h
+++ src/raytri.h
@@ -9,16 +9,20 @@
 
 #include "fixed.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #ifdef FIXED_INTERSECT
 
-extern int fixed_intersect_triangle(
+int fixed_intersect_triangle(
 	fp_t orig[3], fp_t dir[3],
 	fp_t vert0[3], fp_t vert1[3], fp_t vert2[3],
 	fp_t *t, fp_t *u, fp_t *v);
 
 #else
 
-extern int intersect_triangle(
+int intersect_triangle(
 	float orig[3], float dir[3],
 	float vert0[3], float vert1[3], float vert2[3],
 	float *t, float *u, float *v);
@@ -27,21 +31,25 @@ extern int intersect_triangle(
 
 #ifdef INTERSECT_BENCHMARK
 
-extern int intersect_triangle1(
+int intersect_triangle1(
 	float orig[3], float dir[3],
 	float vert0[3], float vert1[3], float vert2[3],
 	float *t, float *u, float *v);
 
-extern int intersect_triangle2(
+int intersect_triangle2(
 	float orig[3], float dir[3],
 	float vert0[3], float vert1[3], float vert2[3],
 	float *t, float *u, float *v);
 
-extern int intersect_triangle3(
+int intersect_triangle3(
 	float orig[3], float dir[3],
 	float vert0[3], float vert1[3], float vert2[3],
 	float *t, float *u, float *v);
 
 #endif //INTERSECT_BENCHMARK
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //RAYTRI_H
